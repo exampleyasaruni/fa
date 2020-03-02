b4777dbb23ba0cc138db12f6201d1c18c58b610a
exampleyasaruni <61406352+exampleyasaruni@users.noreply.github.com>
2020-03-03 00:34:29 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -6,7 +6,7 @@ import java.util.Set;
 import automaton.*;
 
 public class Main {
-	// Change
+	// Change 4
 	public static void main(String[] args) throws IOException {
 		FiniteAutomatonTools tools = new FiniteAutomatonTools();
 		boolean bool = false;
